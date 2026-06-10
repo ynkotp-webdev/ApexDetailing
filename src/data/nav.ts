3099af82cf6c5674c422bb651c14548579bd7885
Ostap Yonka <ynk.otp.nl@gmail.com>
2026-06-10 13:52:37 +0300

    New components have been added and metadata has been updated to improve SEO

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -2,7 +2,7 @@ const base = import.meta.env.BASE_URL;
 
 export const links = [
 	{ name: 'Головна', href: `${base}` },
-	{ name: '#@ Пункт 2', href: '#' },
-	{ name: '#@ Пункт 3', href: '#' },
-	{ name: '#@ Пункт 4', href: '#' },
+	{ name: 'Про нас', href: '#' },
+	{ name: 'Послуги та прайс', href: '#' },
+	{ name: 'Контакти', href: '#' },
 ];
