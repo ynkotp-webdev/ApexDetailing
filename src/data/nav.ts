f63fc1a64511fe95f85c2fb0e81d0942f00bf787
Ostap Yonka <ynk.otp.nl@gmail.com>
2026-06-11 13:38:15 +0300

    feat: Add new images and components for Services and Contacts pages
    - Added new images for the Contacts and Main pages.
    - Created a new Form component for the Contacts page.
    - Implemented an InformationBlock component for the Contacts page.
    - Developed Cards and IntroduceBlock components for the Services page.
    - Added FAQ and WhyOurServices components to enhance the Services page.
    - Updated navigation links to point to the correct sections.
    - Reorganized the layout structure by introducing a new Header and Footer.
    - Removed deprecated components and images to streamline the codebase.
    - Enhanced global styles for better layout and responsiveness.

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -2,7 +2,7 @@ const base = import.meta.env.BASE_URL;
 
 export const links = [
 	{ name: 'Головна', href: `${base}` },
-	{ name: 'Про нас', href: '#' },
-	{ name: 'Послуги та прайс', href: '#' },
-	{ name: 'Контакти', href: '#' },
+	{ name: 'Про нас', href: '#about' },
+	{ name: 'Послуги та прайс', href: `${base}Services` },
+	{ name: 'Контакти', href: `${base}Contacts` },
 ];
