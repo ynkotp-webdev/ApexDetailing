2e2a693e12ed2130a38b4a918f7ca30c4a9c876c
Ostap Yonka <ynk.otp.nl@gmail.com>
2026-06-11 15:49:41 +0300

    Refactor header and navigation components for improved styling and accessibility; update service cards and FAQ section with new layout and design; enhance global styles with new color palette and button variants; create services page and integrate contact component in contacts page.

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -2,7 +2,7 @@ const base = import.meta.env.BASE_URL;
 
 export const links = [
 	{ name: 'Головна', href: `${base}` },
-	{ name: 'Про нас', href: '#about' },
-	{ name: 'Послуги та прайс', href: `${base}Services` },
-	{ name: 'Контакти', href: `${base}Contacts` },
+	{ name: 'Про нас', href: `${base}#about` },
+	{ name: 'Послуги та прайс', href: `${base}services` },
+	{ name: 'Контакти', href: `${base}contacts` },
 ];
